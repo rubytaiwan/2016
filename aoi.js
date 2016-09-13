468db8c11811cdadd954fca31e36dba46583e5ef
蒼時弦也 <elct9620@frost.tw>
2016-09-13 11:08:39 +0800

    Initialize Project

---
+++ aoi.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = function(config, options) {
+
+    config.module.postLoaders = config.module.postLoaders || [];
+    config.module.postLoaders.push({
+        include: path.resolve(__dirname, 'node_modules/pixi.js'),
+        loader: 'ify'
+    });
+
+    config.module.loaders.push({
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?name=[path][name].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+    });
+
+    return config;
+}
---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * RubyConf Taiwan 2016
+ */
+
+import '../sass/app.scss';
+
+(function() {
+    let setup = function() {
+        console.log("Welcome to RubyConf 2016")
+    }
+
+    document.addEventListener('DOMContentLoaded', setup)
+}())
