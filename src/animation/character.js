652251f8c7c70e199293a6941f674474c0e14533
蒼時弦也 <elct9620@frost.tw>
2016-09-19 14:15:45 +0800

    Improve animation layers and skew speed / rate

--- src/animation/character.js
+++ src/animation/character.js
@@ -2,7 +2,7 @@
  * Character
  */
 
-import {Container, Sprite} from 'pixi.js'
+import {BLEND_MODES, Container, Sprite} from 'pixi.js'
 import Resource from './resources'
 import zOrder from 'json!./zorder.json'
 
@@ -12,14 +12,24 @@ export default class Character {
         this.character.pivot.set(692, 0)
         this.character.x = 692
 
-        this.hair = new Sprite(Resource.texture('character-hair'))
+        this.hairFront = new Sprite(Resource.texture('character-hair-front'))
+        this.hairBack = new Sprite(Resource.texture('character-hair-back'))
         this.body = new Sprite(Resource.texture('character-body'))
+        this.gem = new Sprite(Resource.texture('gem'))
 
-        this.hair.zOrder = zOrder['character-hair']
+        this.hairFront.zOrder = zOrder['character-hair-front']
+        this.hairBack.zOrder = zOrder['character-hair-back']
         this.body.zOrder = zOrder['character-body']
+        this.gem.zOrder = zOrder['gem']
 
+        this.gem.pivot.set(570, 0)
+        this.gem.x = 570
+        // this.gem.blendMode = BLEND_MODES.LUMINOSITY
+
+        this.character.addChild(this.hairBack)
         this.character.addChild(this.body)
-        this.character.addChild(this.hair)
+        this.character.addChild(this.hairFront)
+        this.character.addChild(this.gem)
 
         stage.addChild(this.character)
     }
@@ -27,20 +37,29 @@ export default class Character {
     update(currentTime, delta) {
         this.animateHair(currentTime)
         this.animateCharacter(currentTime)
+        this.animateGem(currentTime)
+    }
+
+    animateGem(currentTime) {
+        this.gem.y = Math.sin(currentTime / 1500) * 15 + 10
+        this.gem.skew.set(
+            0,
+            Math.cos(currentTime / 500) * 0.05
+        )
     }
 
     animateCharacter(currentTime) {
-        this.character.y = Math.cos(currentTime / 1000) * 5 + 10
+        this.character.y = Math.cos(currentTime / 750) * 5 + 10
         this.character.skew.set(
             0,
-            Math.sin(currentTime / 1000) * 0.02
+            Math.sin(currentTime / 500) * 0.015
         )
     }
 
     animateHair(currentTime) {
-        this.hair.skew.set(
-            Math.sin(currentTime / 1000 * 2) * 0.005,
-            Math.cos(currentTime / 1000) * 0.001 + 0.002
-        )
+        let skewX = Math.sin(currentTime / 1000 * 2) * 0.005
+        let skewY = Math.cos(currentTime / 500) * 0.001 + 0.002
+        this.hairFront.skew.set(skewX, skewY)
+        this.hairBack.skew.set(skewX * 2, skewY * 2)
     }
 }
